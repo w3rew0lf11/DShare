6f3a635c5b8110307bdd42da5e1bed7072356162
w3rew0lf11 <w3rew0lf11@gmail.com>
2025-06-21 17:01:38 +0545

    Deployed smartcontract ABI integration remaining

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -14,6 +14,35 @@ async function main() {
   // 4. Get the contract address
   const contractAddress = await dshare.getAddress();
   console.log("DShare deployed to:", contractAddress);
+
+  console.log(network.config);
+
+
+  if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for confirmations....");
+    await dshare.deploymentTransaction().wait(6)
+    console.log("Waiting 30 seconds for Etherscan to index the contract...");
+    await new Promise(resolve => setTimeout(resolve, 30000));
+    await verify(dshare.target,[])
+  }
+}
+
+
+//verify function
+async function verify(contractAddress, args) {
+    console.log("Verifying contract....");
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (error) {
+        if (error.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!");
+        } else {
+            console.log(error);
+        }
+    }
 }
 
 // Execute and handle errors
