be3164bcfda1a79913db87947f825e3a017acd18
w3rew0lf11 <w3rew0lf11@gmail.com>
2025-08-07 09:34:25 +0545

    cleaned repo

--- smart-contract/ignition/modules/DShare.js
+++
@@ -1,6 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("DShareModule", (m) => {
-  const dshare = m.contract("DShare");
-  return { dshare };
-});
